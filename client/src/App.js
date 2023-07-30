1f82f6d1c576e5a27fd0fa7538971154aa96acef
Aditi Jain <aditijain1100@gmail.com>
2023-07-30 16:07:05 +0530

    react app initialized

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eCom!</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
