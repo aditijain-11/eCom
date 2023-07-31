4b46340425e097a90fa96071a8305b8938b1871f
Aditi Jain <aditijain1100@gmail.com>
2023-07-31 07:08:57 +0530

    Create signin api tested on Postman, Generate token function implementation

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 mongoose
@@ -16,10 +17,18 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
-// backend api for returning product based on it's slug id
+app.use('/api/users', userRouter);
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+// backend api for returning product based on it's slug id
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
