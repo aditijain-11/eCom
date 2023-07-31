21d7a3888b1cffa0f11ceadb784d140248b85163
Aditi Jain <aditijain1100@gmail.com>
2023-07-31 06:01:45 +0530

    creating sample users/admin

--- server/data.js
+++ server/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Aditi',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jacob',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
